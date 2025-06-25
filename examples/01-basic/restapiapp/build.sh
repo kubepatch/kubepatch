d8f976af8f93f5b52773e2d140fdae9dab947240
alexey.zh <mailbox.stdio@gmail.com>
2025-06-25 08:58:08 +0500

    test: working on localdev env

--- examples/01-basic/restapiapp/build.sh
+++ examples/01-basic/restapiapp/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+docker buildx build -t localhost:5000/restapiapp .
 docker buildx build -t localhost:5000/restapiapp:1.21 .
 docker buildx build -t localhost:5000/restapiapp:1.22 .
 docker push localhost:5000/restapiapp:1.21
