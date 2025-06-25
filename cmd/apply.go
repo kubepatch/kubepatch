0df4a6e004c5ebfeecb069637001f24d042be627
alexey.zh <mailbox.stdio@gmail.com>
2025-06-25 10:41:33 +0500

    labels - feat: cobra CLI

---
+++ cmd/apply.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"time"
+
+	"github.com/hashmap-kz/kubepatch/internal/patch"
+
+	"github.com/spf13/cobra"
+)
+
+type PatchCmdOptions struct {
+	Filenames     []string
+	PatchFilePath string
+	Timeout       time.Duration
+	Recursive     bool
+}
+
+func NewPatchCmd() *cobra.Command {
+	opts := PatchCmdOptions{}
+	cmd := &cobra.Command{
+		Use:           "patch",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return patch.Run(opts.Filenames, opts.PatchFilePath)
+		},
+	}
+	cmd.Flags().StringSliceVarP(&opts.Filenames, "filename", "f", nil, "Manifest files, glob patterns, or directories to apply.")
+	cmd.Flags().StringVarP(&opts.PatchFilePath, "patchfile", "p", "", "Patch file")
+	//nolint:errcheck
+	_ = cmd.MarkFlagRequired("filename")
+	cmd.Flags().BoolVarP(&opts.Recursive, "recursive", "R", false, "Recurse into directories specified with --filename.")
+	return cmd
+}
